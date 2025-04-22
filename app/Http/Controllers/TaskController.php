b0765eb396e762428d0ae048057785d536aae7a7
dev_azi <azi26.dev@gmail.com>
2025-04-22 11:26:26 +0800

    first commit

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all(); // Fetch all tasks
+        $tasks = Task::all();
         return view('tasks.index', compact('tasks'));
     }
 
